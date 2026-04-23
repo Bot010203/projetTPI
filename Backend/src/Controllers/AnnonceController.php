26aad94870ae91724cf9ebfe24a2221e3f411acd
paulchcch <paul.chcch@eduge.ch>
2026-04-23 15:33:08 +0200

    Ajout du fichier AnnonceController et mise à jour de web.php

---
+++ Backend/src/Controllers/AnnonceController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+class AnnonceController
+{
+    
+}
--- Backend/src/Routes/Web.php
+++ Backend/src/Routes/Web.php
@@ -5,11 +5,12 @@ use Slim\App;
 final class Web {
     public static function register(App $app): void {
         $app->get('/', function ($request, $response) {
-            $response->getBody()->write('Bienvenue sur l\'API de votre projet TPI !');
+            $response->getBody()->write('Bienvenu');
             return $response;
         });
         $app->post('/login', [AuthController::class, 'login']);
         $app->post('/register', [AuthController::class, 'register']);
+        
     }
 }
 
